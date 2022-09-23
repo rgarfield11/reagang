ca56aa1d3fe0cb66d6397beb2eaf416e8cae5d3a
Ross <rossgarfield@gmail.com>
2022-09-22 20:03:30 -0500

    contact styling

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,11 +6,11 @@ function Contact() {
         <div className="contact-container">
             <h1>Contact Me</h1>
             <form className="contact-form">
-                <input className="form-info" type="text" placeholder="Full Name"  name="user_name" required />
+                <input className="form-info" type="text" placeholder="Name..."  name="user_name" required />
                 <br/>
-                <input className="form-info" type="email" placeholder="Email Address" name="user_email" required />
+                <input className="form-info" type="email" placeholder="Email Address..." name="user_email" required />
                 <br/>
-                <input className="form-info" type="text" placeholder="Subject" name="subject" required />
+                <input className="form-info" type="text" placeholder="Subject..." name="subject" required />
                 <br/>
                 <textarea className="form-message" placeholder="Write your message here..." name="message"></textarea>
                 <br/>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,10 +5,12 @@ import Reagan from '../Reagang.jpg'
 function Home() {
   return (
     <div className="home">
+      <div className="home-name">
+        <h1>Reagan</h1>
+        <h1>McCann</h1>
+        <h1>Garvin</h1>
+      </div>
       <img src={Reagan} alt="Reagan McCann Gavin" className="profile" />
-      <h1>Reagan</h1>
-      <h3>McCann</h3>
-      <h1>Garvin</h1>
       {/* <h1>Reagan McCann Garvin</h1> */}
     </div>
   )
